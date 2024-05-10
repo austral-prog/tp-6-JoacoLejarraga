54a6aee1a762a2fdf6a997cb8c17a44c487cb737
JoacoLejarraga <162721835+JoacoLejarraga@users.noreply.github.com>
2024-05-10 12:16:55 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,29 +1,33 @@
-def remove_elements(lista):
-	del lista[0]
-	del lista [3]
-	del lista [3]
-	return lista
+def remove_elements(list1):
+	del list1[0:1]
+	del list1[3:4]
+	del list1[3:4]
+	return list1
 
-def add_elements(lista):
-	lista.insert(0, "Pink")
-	lista.append("Yellow")
-	return lista
+def add_elements(list2):
+	list2.insert(0, "Pink")
+	list2.append("Yellow")
+	return list2
 
-def is_empty(lista):
-	if lista:
+def is_empty(list3):
+	if list3:
 		return False
 	else:
 		return True
 
-def check_lists(lista, lista2):
-	if lista[2] == lista2[2]:
-		return True
-	else:
+def check_lists(list4, list5):
+	if is_empty(list4) or is_empty(list5):
 		return False
+	else:
+		if list4[2:3] == list5[2:3]:
+			return True
+		else:
+			return False
 
-def list_of_lists(lista1, lista2, lista3):
-	new_lista1 = lista1[0:2]
-	new_lista2 = lista2[1:4]
-	new_lista3 = lista3[-2:]
+def list_of_lists(lista):
+	new_lista1 = lista[0][0:2]
+	new_lista2 = lista[1][1:4]
+	new_lista3 = lista[2][-2:]
 	lista_externa = [new_lista1, new_lista2, new_lista3]
 	return lista_externa
+
